9f27920e279ec178e646207c2cec2e5457065cbb
Veronika Ferencikova <V.Strbikova@seznam.cz>
2023-03-07 09:36:14 +0100

    Zadani z ucebnice

---
+++ Lekce_71_cestovatelsky_denik.py
@@ -0,0 +1,13 @@
+# cetovatelský deník
+travel_diary = [
+    {
+        "country": "Spain",
+        "visited_cities": ["Madrid", "Leon", "Valencia"],
+        "visits": 5
+    },
+    {
+        "country": "France",
+        "visited_cities": ["Paris", "Nice", "Rennes"],
+        "visits": 2
+    }
+]
